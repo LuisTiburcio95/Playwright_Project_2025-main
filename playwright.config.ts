b98556e1968b65bed4378794275c5393f32e018e
LuisTiburcio95 <dariel_tiburcio01@hotmail.com>
2025-12-12 22:15:08 -0500

    PlaywrightActionItem1

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,9 +26,6 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
     actionTimeout: 15000, // your time limit
     screenshot: 'only-on-failure',
     video: 'on',
